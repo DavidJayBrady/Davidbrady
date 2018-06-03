f2349d7cbd45fb44dc2fe55a935e77eac5b25f68
David Brady <djbrady@uci.edu>
2018-06-02 20:57:49 -0700

    initial. only has background pic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import cog from "./Assets/backgroundcog.jpg";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <img src={cog}/> /**/
+      </div>
+    );
+  }
+}
+
+export default App;
