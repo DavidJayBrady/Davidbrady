940609213f1b43ccc668f69820416f32cc675c5d
David Brady <djbrady@uci.edu>
2018-08-13 12:38:34 -0700

    compiled with router in

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import Routes from '.';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 ReactDOM.render(Routes, document.getElementById('root'));
 registerServiceWorker();
+*/
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Routes from './Routes/routes';
+
+if ( process.env.NODE_ENV !== 'production') {
+	window.React = React;
+}
+
+ReactDOM.render(Routes, document.getElementById('root'));
+
--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import {Router, Route, IndexRoute} from 'react-router';
-import App from './App.js';
-
-
-export default (
-	<Router component={App}>
-	</Router>
-)
